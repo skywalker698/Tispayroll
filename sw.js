23ef70f9eb952e7b3c2e9e13201a547e83cca275
skywalker698 <135288625+skywalker698@users.noreply.github.com>
2026-09-19 14:37:36 +0700

    Update icon paths in service worker

--- sw.js
+++ sw.js
@@ -9,11 +9,11 @@ const SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-maskable-512.png',
-  './icons/apple-touch-icon.png',
-  './icons/favicon-48.png'
+  './icon-192.png',
+  './icon-512.png',
+  './icon-maskable-512.png',
+  './apple-touch-icon.png',
+  './favicon-48.png'
 ];
 
 self.addEventListener('install', (event) => {
